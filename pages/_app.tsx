33269687b35d6f5ab430dcb197b3ec3cfd3e1092
tsing-dong <ld320321@163.com>
2020-05-11 08:07:27 +0800

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,29 @@ import React from 'react';
 import App, {Container} from 'next/app';
 import LayoutComponent from '../components/layout';
 import "antd/dist/antd.css";
+import loggerMiddleware from 'redux-logger';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/index';
 
+//中间件
+const logger = store => next => action => {
+    console.log('dispatch->',action);
+    let result = next(action); // 加载下一个中间件
+    console.log('next state->',store.getState());
+    return result;
+};
 
-const store = createStore(rootReducer);
+const error = store => next => action => {
+    try {
+        next(action)
+    } catch (error) {
+        console.log('error->',error);
+    }
+};
+
+
+const store = createStore(rootReducer,{},applyMiddleware(logger,error,loggerMiddleware));
 
 export default class MyApp extends App {
 
